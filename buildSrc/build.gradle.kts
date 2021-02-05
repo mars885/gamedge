45a569c0471987fd912198f6ff309b4b9aff7fbd
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2021-02-05 18:05:08 +0200

    Update Gradle plugin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -28,7 +28,7 @@ kotlinDslPluginOptions {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.1.1")
+    implementation("com.android.tools.build:gradle:4.1.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
     implementation(gradleApi()) // for custom plugins
 }
