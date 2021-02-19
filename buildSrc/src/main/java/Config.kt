5b22b8a040c8a98bc6854644d8bcd3c4d8f19477
Paul Rybitskyi <Paul.Rybitskyi@gmail.com>
2021-02-19 15:57:52 +0200

    Bump the compileSdkVersion to 30 (#24)

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -21,10 +21,10 @@ import org.gradle.api.JavaVersion
 
 object appConfig {
 
-    const val compileSdkVersion = 29
-    const val targetSdkVersion = 29
+    const val compileSdkVersion = 30
+    const val targetSdkVersion = 30
     const val minSdkVersion = 21
-    const val buildToolsVersion = "29.0.2"
+    const val buildToolsVersion = "29.0.3"
     const val applicationId = "com.paulrybitskyi.gamedge"
     const val versionCode = 1
     const val versionName = "1.0.0"
