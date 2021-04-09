b84e611fb7c04ece4139c910c51df31bf12f4362
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2021-04-10 00:34:51 +0300

    Add Job as return type of wrapper methods

--- commons-ui/src/main/java/com/paulrybitskyi/gamedge/commons/ui/OneShotEventUtils.kt
+++ commons-ui/src/main/java/com/paulrybitskyi/gamedge/commons/ui/OneShotEventUtils.kt
@@ -20,16 +20,17 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.coroutineScope
 import androidx.lifecycle.flowWithLifecycle
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.launchIn
 
 
-fun <T> Flow<T>.observeIn(lifecycleOwner: LifecycleOwner) {
-    flowWithLifecycle(lifecycleOwner.lifecycle)
+fun <T> Flow<T>.observeIn(lifecycleOwner: LifecycleOwner): Job {
+    return flowWithLifecycle(lifecycleOwner.lifecycle)
         .launchIn(lifecycleOwner.lifecycle.coroutineScope)
 }
 
 
-fun <T> Flow<T>.observeIn(fragment: Fragment) {
-    observeIn(fragment.viewLifecycleOwner)
+fun <T> Flow<T>.observeIn(fragment: Fragment): Job {
+    return observeIn(fragment.viewLifecycleOwner)
 }
