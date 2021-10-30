84cb269388be8dcc3a7c6808e37d720cfef3edab
Paul Rybitskyi <Paul.Rybitskyi@gmail.com>
2021-10-30 21:04:25 +0300

    Revert "Bump Java target version to 11 (#126)" (#127)
    This reverts commit 5c4e905426bde931755cdf336e857312c883ec7c.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -28,8 +28,8 @@ object appConfig {
     const val versionCode = 1
     const val versionName = "1.0.0"
 
-    val javaCompatibilityVersion = JavaVersion.VERSION_11
-    val kotlinCompatibilityVersion = JavaVersion.VERSION_11
+    val javaCompatibilityVersion = JavaVersion.VERSION_1_8
+    val kotlinCompatibilityVersion = JavaVersion.VERSION_1_8
 
 }
 
