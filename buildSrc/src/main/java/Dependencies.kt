65776e17f60b5f84cbade08dfc7c9619ecfac3ad
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2021-10-30 17:11:46 +0300

    Fix build errors

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -21,8 +21,8 @@ import org.gradle.api.JavaVersion
 
 object appConfig {
 
-    const val compileSdkVersion = 30
-    const val targetSdkVersion = 30
+    const val compileSdkVersion = 31
+    const val targetSdkVersion = 31
     const val minSdkVersion = 21
     const val buildToolsVersion = "30.0.2"
     const val applicationId = "com.paulrybitskyi.gamedge"
