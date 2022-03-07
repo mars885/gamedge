f073e0fccc103212e819a3edb89c466043fa3a65
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-03-07 22:52:17 +0200

    Bump AssertJ to 3.22.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -197,7 +197,7 @@ object deps {
 
         private const val jUnitVersion = "4.13.2"
         private const val jUnitExtVersion = "1.1.3"
-        private const val assertJVersion = "3.21.0"
+        private const val assertJVersion = "3.22.0"
         private const val mockkVersion = "1.12.0"
         private const val turbineVersion = "0.7.0"
         private const val testRunnerVersion = "1.4.0"
