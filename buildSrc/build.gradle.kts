26ba29293bb713b0ec28de251a98fe09def6039a
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-03-07 21:40:41 +0200

    Fix build errors

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,6 +25,10 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.1.2")
+    // https://github.com/google/dagger/issues/3068#issuecomment-999118496
+    // Needs to be checked whether JavaPoet is still needed after AGP is updated
+    // because currently it forces 1.10 JavaPoet version, for some odd reason.
+    implementation("com.squareup:javapoet:1.13.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation(gradleApi()) // for custom plugins
 }
