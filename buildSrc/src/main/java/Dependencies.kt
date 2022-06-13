661bd6a03853d7bea29b8b8cfddc5f2d0290b698
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-06-13 17:09:42 +0300

    Bump turbine lib to 0.8.0 version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -232,7 +232,7 @@ object deps {
         private const val jUnitExtVersion = "1.1.3"
         private const val assertJVersion = "3.22.0"
         private const val mockkVersion = "1.12.3"
-        private const val turbineVersion = "0.7.0"
+        private const val turbineVersion = "0.8.0"
         private const val testRunnerVersion = "1.4.0"
         private const val archCoreVersion = "2.1.0"
         private const val mockWebServerVersion = "4.9.2"
