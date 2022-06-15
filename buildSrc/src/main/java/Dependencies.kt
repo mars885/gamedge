216cb21fd6020b79656904003c0025ecebbffea2
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-06-16 00:50:36 +0300

    Bump mockK to 1.12.4 version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -193,7 +193,7 @@ object deps {
         private const val jUnitVersion = "4.13.2"
         private const val jUnitExtVersion = "1.1.3"
         private const val assertJVersion = "3.23.1"
-        private const val mockkVersion = "1.12.3"
+        private const val mockkVersion = "1.12.4"
         private const val turbineVersion = "0.8.0"
         private const val testRunnerVersion = "1.4.0"
         private const val archCoreVersion = "2.1.0"
