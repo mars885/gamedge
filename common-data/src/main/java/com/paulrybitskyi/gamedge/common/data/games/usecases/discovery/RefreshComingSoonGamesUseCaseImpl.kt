9aa668f4d2c37c47aeaf4352da998527795ea932
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-07-24 02:44:25 +0300

    Give implicit it parameter a name when using onEachSuccess operator

--- common-data/src/main/java/com/paulrybitskyi/gamedge/common/data/games/usecases/discovery/RefreshComingSoonGamesUseCaseImpl.kt
+++ common-data/src/main/java/com/paulrybitskyi/gamedge/common/data/games/usecases/discovery/RefreshComingSoonGamesUseCaseImpl.kt
@@ -52,8 +52,8 @@ internal class RefreshComingSoonGamesUseCaseImpl @Inject constructor(
                 emit(gamesDataStores.remote.getComingSoonGames(params.pagination.toDataPagination()))
             }
         }
-        .onEachSuccess {
-            gamesDataStores.local.saveGames(it)
+        .onEachSuccess { games ->
+            gamesDataStores.local.saveGames(games)
             throttlerTools.throttler.updateGamesLastRefreshTime(throttlerKey)
         }
         .flowOn(dispatcherProvider.main)
