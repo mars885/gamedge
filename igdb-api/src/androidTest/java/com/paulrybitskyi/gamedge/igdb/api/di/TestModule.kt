a0b9b7a54c4597441a5ac19332526a7c88f7a917
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-08-01 22:45:29 +0300

    Fix igdb-api instrumentation tests

--- igdb-api/src/androidTest/java/com/paulrybitskyi/gamedge/igdb/api/di/TestModule.kt
+++ igdb-api/src/androidTest/java/com/paulrybitskyi/gamedge/igdb/api/di/TestModule.kt
@@ -17,10 +17,17 @@
 package com.paulrybitskyi.gamedge.igdb.api.di
 
 import com.paulrybitskyi.gamedge.common.testing.di.MocksModule
+import com.paulrybitskyi.gamedge.igdb.api.common.CredentialsStore
+import com.paulrybitskyi.gamedge.igdb.api.utils.FakeCredentialsStore
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module(includes = [MocksModule::class])
 @InstallIn(SingletonComponent::class)
-internal object TestModule
+internal interface TestModule {
+
+    @Binds
+    fun bind(binding: FakeCredentialsStore): CredentialsStore
+}
