e05b9cb1c2ee34bbda1ac93a1fcb557977d0d36f
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-08-03 23:26:28 +0300

    Bump Zoomable to 1.5.1 version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -181,7 +181,7 @@ object deps {
         private const val kotlinResultVersion = "1.1.16"
         private const val hiltBinderVersion = "1.1.2"
         private const val coilVersion = "2.1.0"
-        private const val zoomableVersion = "1.4.2"
+        private const val zoomableVersion = "1.5.1"
 
         const val desugaredLibs = "com.android.tools:desugar_jdk_libs:${desugaredLibsVersion}"
         const val kotlinResult = "com.michael-bull.kotlin-result:kotlin-result:${kotlinResultVersion}"
