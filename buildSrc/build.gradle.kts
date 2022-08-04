496ed3453aed35bc812ee8f4449882a5d0a6402f
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-08-04 20:55:52 +0300

    Bump Protobuf to 0.8.19 inside buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -30,6 +30,6 @@ dependencies {
     // because currently it forces 1.10 JavaPoet version, for some odd reason.
     implementation("com.squareup:javapoet:1.13.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
+    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.19")
     implementation(gradleApi()) // for custom plugins
 }
