756da02d40c41ac45cc4f0519a4fac1be745ceaa
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-08-04 03:10:08 +0300

    Return result monad from use cases where appropriate

--- feature-info/src/main/java/com/paulrybitskyi/gamedge/feature/info/domain/usecases/GetCompanyDevelopedGamesUseCase.kt
+++ feature-info/src/main/java/com/paulrybitskyi/gamedge/feature/info/domain/usecases/GetCompanyDevelopedGamesUseCase.kt
@@ -16,9 +16,10 @@
 
 package com.paulrybitskyi.gamedge.feature.info.domain.usecases
 
+import com.github.michaelbull.result.Ok
 import com.paulrybitskyi.gamedge.common.domain.common.DispatcherProvider
+import com.paulrybitskyi.gamedge.common.domain.common.DomainResult
 import com.paulrybitskyi.gamedge.common.domain.common.entities.Pagination
-import com.paulrybitskyi.gamedge.common.domain.common.extensions.resultOrError
 import com.paulrybitskyi.gamedge.common.domain.common.usecases.UseCase
 import com.paulrybitskyi.gamedge.common.domain.games.datastores.GamesLocalDataStore
 import com.paulrybitskyi.gamedge.common.domain.games.entities.Company
@@ -29,11 +30,12 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 import javax.inject.Singleton
 
-internal interface GetCompanyDevelopedGamesUseCase : UseCase<Params, Flow<List<Game>>> {
+internal interface GetCompanyDevelopedGamesUseCase : UseCase<Params, Flow<DomainResult<List<Game>>>> {
 
     data class Params(
         val company: Company,
@@ -49,14 +51,14 @@ internal class GetCompanyDevelopedGamesUseCaseImpl @Inject constructor(
     private val dispatcherProvider: DispatcherProvider,
 ) : GetCompanyDevelopedGamesUseCase {
 
-    override suspend fun execute(params: Params): Flow<List<Game>> {
+    override suspend fun execute(params: Params): Flow<DomainResult<List<Game>>> {
         return refreshCompanyDevelopedGamesUseCase
             .execute(RefreshCompanyDevelopedGamesUseCase.Params(params.company, params.pagination))
-            .resultOrError()
             .onEmpty {
                 val localCompanyDevelopedGamesFlow = flow {
                     emit(gamesLocalDataStore.getCompanyDevelopedGames(params.company, params.pagination))
                 }
+                .map<List<Game>, DomainResult<List<Game>>>(::Ok)
 
                 emitAll(localCompanyDevelopedGamesFlow)
             }
