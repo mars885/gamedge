e40d636e19285467cc4f1f8c2191792b8baf6367
Paul Rybitskyi <Paul.Rybitskyi@gmail.com>
2024-08-24 23:17:20 +0300

    Use primitive mutableStateOf where possible (#231)

--- common-ui-widgets/src/main/java/com/paulrybitskyi/gamedge/common/ui/widgets/games/Game.kt
+++ common-ui-widgets/src/main/java/com/paulrybitskyi/gamedge/common/ui/widgets/games/Game.kt
@@ -25,7 +25,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -112,7 +112,7 @@ private fun Details(
 
 @Composable
 private fun DetailsDescription(description: String) {
-    var maxLines by rememberSaveable { mutableStateOf(Int.MAX_VALUE) }
+    var maxLines by rememberSaveable { mutableIntStateOf(Int.MAX_VALUE) }
 
     Text(
         text = description,
