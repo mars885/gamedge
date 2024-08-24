ea91dbc2aaeb086a6459847d4f1da7f5f418b5f4
mars885 <paul.rybitskyi.work@gmail.com>
2024-08-24 22:33:44 +0300

    Remove accompanist system ui controller lib

--- buildSrc/src/main/java/com/paulrybitskyi/gamedge/plugins/GamedgeJetpackComposePlugin.kt
+++ buildSrc/src/main/java/com/paulrybitskyi/gamedge/plugins/GamedgeJetpackComposePlugin.kt
@@ -25,6 +25,5 @@ class GamedgeJetpackComposePlugin : Plugin<Project> {
         add("implementation", libs.composeRuntime.get())
         add("implementation", libs.composeAnimation.get())
         add("implementation", libs.composeConstraintLayout.get())
-        add("implementation", libs.accompanistSystemUi.get())
     }
 }
