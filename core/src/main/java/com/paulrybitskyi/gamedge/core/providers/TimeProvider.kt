bd495229a796386c95f29c056cf2c3147348e3f5
mars885 <github@paulrybitskyi.com>
2025-02-20 21:10:07 +0200

    Update contact email

--- core/src/main/java/com/paulrybitskyi/gamedge/core/providers/TimeProvider.kt
+++ core/src/main/java/com/paulrybitskyi/gamedge/core/providers/TimeProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Paul Rybitskyi, paul.rybitskyi.work@gmail.com
+ * Copyright 2021 Paul Rybitskyi, oss@paulrybitskyi.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
