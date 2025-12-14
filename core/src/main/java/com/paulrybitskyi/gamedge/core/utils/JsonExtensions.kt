e7d1655d3bacffb53f60089241f975328296d020
mars885 <github@paulrybitskyi.com>
2025-12-14 19:31:22 +0200

    Fix Room build warning

--- core/src/main/java/com/paulrybitskyi/gamedge/core/utils/JsonExtensions.kt
+++ core/src/main/java/com/paulrybitskyi/gamedge/core/utils/JsonExtensions.kt
@@ -21,7 +21,7 @@ import kotlinx.serialization.json.Json
 inline fun <reified T> Json.decodeFromStringOrNull(json: String): T? {
     return try {
         decodeFromString(json)
-    } catch (ignore: Throwable) {
+    } catch (_: Throwable) {
         null
     }
 }
